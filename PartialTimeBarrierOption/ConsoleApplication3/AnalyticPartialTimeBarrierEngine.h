bde629f0cf6623ecd420e8f15653d0c5380d9519
Ahmed Ayadi <ahmed.ayadi@esprit.tn>
2014-01-09 16:03:45 +0100

    commit error fix

--- PartialTimeBarrierOption/ConsoleApplication3/AnalyticPartialTimeBarrierEngine.h
+++ PartialTimeBarrierOption/ConsoleApplication3/AnalyticPartialTimeBarrierEngine.h
@@ -1,4 +1,3 @@
-
 #include "PartialTimeBarrierOption.h"
 #include <ql/instruments/barrieroption.hpp>
 #include <ql/processes/blackscholesprocess.hpp>
@@ -6,38 +5,43 @@
 
 namespace QuantLib {
 
-	class AnalyticPartialTimeBarrierEngine : public PartialTimeBarrierOption::engine {
-	public:
-		AnalyticPartialTimeBarrierEngine(
-			const boost::shared_ptr<GeneralizedBlackScholesProcess>& process);
-		void calculate() const;
-	private:
-		boost::shared_ptr<GeneralizedBlackScholesProcess> process_;
-		CumulativeNormalDistribution f_;
-		Real underlying() const;
-		Real strike() const;
-		Time residualTime() const;
-		Time coverEventDate() const;
-		Volatility volatility() const;
-		Real barrier() const;
-		Real rebate() const;
-		Real stdDeviation() const;
-		Rate riskFreeRate() const;
-		DiscountFactor riskFreeDiscount() const;
-		Rate dividendYield() const;
-		DiscountFactor dividendDiscount() const;
-		Real M(Real a,Real b,Real ro,Integer n) const;
-		Real d1()const;
-		Real d2()const;
-		Real e1() const;
-		Real e2() const;
-		Real e3() const;
-		Real e4() const;
-		Real f1() const;
-		Real f2() const;
-		Real ro() const;
-		Rate mu() const;
-		Real CA(Real strike,Integer n) const;
-		Real CIA(Integer n) const;
-	};
+        class AnalyticPartialTimeBarrierEngine : public PartialTimeBarrierOption::engine {
+        public:
+                AnalyticPartialTimeBarrierEngine(
+                        const boost::shared_ptr<GeneralizedBlackScholesProcess>& process);
+                void calculate() const;
+        private:
+                boost::shared_ptr<GeneralizedBlackScholesProcess> process_;
+                CumulativeNormalDistribution f_;
+                Real underlying() const;
+                Real strike() const;
+                Time residualTime() const;
+                Time coverEventDate() const;
+                Volatility volatility() const;
+                Real barrier() const;
+                Real rebate() const;
+                Real stdDeviation() const;
+                Rate riskFreeRate() const;
+                DiscountFactor riskFreeDiscount() const;
+                Rate dividendYield() const;
+                DiscountFactor dividendDiscount() const;
+                Real M(Real a,Real b,Real ro,Integer n) const;
+                Real d1()const;
+                Real d2()const;
+                Real e1() const;
+                Real e2() const;
+                Real e3() const;
+                Real e4() const;
+                Real f1() const;
+                Real f2() const;
+                Real ro() const;
+                Rate mu() const;
+                Real CoB2(PartialBarrier::Type barrierType) const;
+                Real CoB1() const;
+                Real CA(Integer n) const;
+                Real g1()const;
+                Real g2()const;
+                Real g3()const;
+                Real g4()const;
+        };
 }
